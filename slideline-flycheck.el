114d369ae8eb9bc5604d53caa783300181069fdf
Jen-Chieh Shen <jcs090218@gmail.com>
2022-06-14 17:17:13 +0800

    add src

--- slideline-flycheck.el
+++ slideline-flycheck.el
@@ -1,8 +1,40 @@
-;; ========================================================================
-;; $File: slideline-flycheck.el $
-;; $Date: 2022-06-14 17:08:50 $
-;; $Revision: $
-;; $Creator: Jen-Chieh Shen $
-;; $Notice: See LICENSE.txt for modification and distribution information
-;;                   Copyright © 2022 by Shen, Jen-Chieh $
-;; ========================================================================
+;;; slideline-flycheck.el --- Show flycheck errors with sideline  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Shen, Jen-Chieh
+;; Created date 2022-06-14 17:10:48
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Description: Show flycheck errors with sideline
+;; Keyword: sideline flycheck
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "26.1"))
+;; URL: https://github.com/jcs-elpa/slideline-flycheck
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Show flycheck errors with sideline.
+;;
+
+;;; Code:
+
+(require 'sideline)
+
+
+
+(provide 'slideline-flycheck)
+;;; slideline-flycheck.el ends here

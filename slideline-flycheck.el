814fd5e2c4977168a77ef191175fe2a78f7b741f
Jen-Chieh Shen <jcs090218@gmail.com>
2022-06-14 17:09:43 +0800

    add src

---
+++ slideline-flycheck.el
@@ -0,0 +1,8 @@
+;; ========================================================================
+;; $File: slideline-flycheck.el $
+;; $Date: 2022-06-14 17:08:50 $
+;; $Revision: $
+;; $Creator: Jen-Chieh Shen $
+;; $Notice: See LICENSE.txt for modification and distribution information
+;;                   Copyright © 2022 by Shen, Jen-Chieh $
+;; ========================================================================
